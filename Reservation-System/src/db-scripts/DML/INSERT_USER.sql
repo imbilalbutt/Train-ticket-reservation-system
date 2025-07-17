de7c511a0fffe883f7ee94911fbf0f9542550b2c
Bilal Ahmad Butt <54913211+imbilalbutt@users.noreply.github.com>
2025-07-17 18:08:50 +0200

    minor changes

--- Reservation-System/src/db-scripts/DML/INSERT_USER.sql
+++ Reservation-System/src/db-scripts/DML/INSERT_USER.sql
@@ -17,6 +17,47 @@ INSERT INTO users(
       reconfirm_password,
       roles,
       username)
+Values(1,
+       'SQL',
+       '2022-04-09T18:00:00.000',
+       true, 'SQL',
+       '2022-04-09T18:00:00.000',
+       true,
+       '+13040000100',
+       '800007000001',
+       'email@yahoo.com',
+       'Bilal Ahmad',
+       'Home Address Road',
+       'Butt',
+--        '$2a$12$9INYZXwgGFjMBNxAw.nFeelD4CuCiHXzECMkTPnKnK3NsLIYzdumW',
+--        '$2a$12$9INYZXwgGFjMBNxAw.nFeelD4CuCiHXzECMkTPnKnK3NsLIYzdumW',
+
+--     password@123
+    '$2a$12$lzVAKxOZEMyzTqLE.SIacux6UQASzJjWP/VD29Dcx6dy.bovclBgC',
+       '$2a$12$lzVAKxOZEMyzTqLE.SIacux6UQASzJjWP/VD29Dcx6dy.bovclBgC',
+       'user',
+       'imbilalbutt');
+
+
+
+INSERT INTO users(
+    user_id,
+    created_by,
+    created_date,
+    is_non_locked,
+    modified_by,
+    modified_date,
+    active_status,
+    cell_number,
+    cnic_number,
+    email,
+    first_name,
+    home_address,
+    last_name,
+    password,
+    reconfirm_password,
+    roles,
+    username)
 Values(2,
        'SQL',
        '2022-04-09T18:00:00.000',
@@ -33,7 +74,7 @@ Values(2,
 --        '$2a$12$9INYZXwgGFjMBNxAw.nFeelD4CuCiHXzECMkTPnKnK3NsLIYzdumW',
 
 --     password@123
-    '$2a$12$lzVAKxOZEMyzTqLE.SIacux6UQASzJjWP/VD29Dcx6dy.bovclBgC',
+       '$2a$12$lzVAKxOZEMyzTqLE.SIacux6UQASzJjWP/VD29Dcx6dy.bovclBgC',
        '$2a$12$lzVAKxOZEMyzTqLE.SIacux6UQASzJjWP/VD29Dcx6dy.bovclBgC',
        'user',
        'imbilalbutt2');
