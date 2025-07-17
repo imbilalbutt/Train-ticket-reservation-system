609f16bca37d83432e89d1eeaf08816aba29b4cc
Bilal Ahmad Butt <54913211+imbilalbutt@users.noreply.github.com>
2025-07-17 17:59:16 +0200

    Deployment in Docker works
    Problem was that I was setting port in application.properties and JettyCustomizer.java

--- Reservation-System/src/main/java/pk/imbilalbutt/bussiness/configuration/JettyCustomizer.java
+++ Reservation-System/src/main/java/pk/imbilalbutt/bussiness/configuration/JettyCustomizer.java
@@ -1,17 +1,17 @@
-package pk.imbilalbutt.bussiness.configuration;
-
-import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.stereotype.Component;
-
-@Component
-public class JettyCustomizer implements WebServerFactoryCustomizer<JettyServletWebServerFactory> {
-
-    @Override
-    public void customize(JettyServletWebServerFactory factory) {
-
-        factory.setPort(9090);
-        factory.setContextPath("/Reservation-System");
-        // Add any additional Jetty configurations if needed
-    }
-}
+//package pk.imbilalbutt.bussiness.configuration;
+//
+//import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+//import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class JettyCustomizer implements WebServerFactoryCustomizer<JettyServletWebServerFactory> {
+//
+//    @Override
+//    public void customize(JettyServletWebServerFactory factory) {
+//
+//        factory.setPort(4000); // Port inside  DOCKER container, should match with Dockerfile EXPOSE and docker-compose SERVER PORT
+//        factory.setContextPath("/Reservation-System");
+//        // Add any additional Jetty configurations if needed
+//    }
+//}
